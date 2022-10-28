230638d4c71fda6f97cccf5891b168b716dd6fcf
Mayank <mayank.srmu@gmail.com>
2022-10-28 20:57:58 +0530

    posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useInfiniteQuery } from "react-query";
-import { fetchPosts } from "utils/apiUtils";
+import { fetchPosts, postType } from "utils/apiUtils";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const { data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage } =
-    useInfiniteQuery("repos", ({ pageParam = 1 }) => fetchPosts(pageParam), {
-      getNextPageParam: (lastPage, allPages) => {
-        const nextPage = allPages?.length + 1;
-        return lastPage?.items?.length !== 0 ? nextPage : undefined;
-      },
-    });
+    useInfiniteQuery<postType[]>(
+      "repos",
+      ({ pageParam = 1 }) => fetchPosts(pageParam),
+      {
+        getNextPageParam: (lastPage, allPages) => {
+          const nextPage = allPages?.length + 1;
+          return lastPage?.length !== 0 ? nextPage : undefined;
+        },
+      }
+    );
+  const [filterBy, setFilterBy] = useState("");
+  const [sortPosts, setSort] = useState(false);
+  const [selected, setSelected] = useState(-1);
   const observerElem = useRef(null);
   const handleObserver = useCallback(
     (entries: IntersectionObserverEntry[]) => {
@@ -32,23 +39,65 @@ export default function Home() {
     console.log({ data });
     return () => observer.unobserve(element);
   }, [fetchNextPage, hasNextPage, handleObserver]);
-  return (
-    <div className="container">
-      <p className="bg-red-500">Hi Tailwind</p>
-      <ul>
-        {isSuccess &&
-          data.pages?.map((page) =>
-            page?.map((item: any, i: number) => (
-              <li key={i}>
-                <pre>{JSON.stringify(item, null, 2)}</pre>
-              </li>
-            ))
-          )}
 
-        <div className="loader" ref={observerElem}>
-          {isFetchingNextPage && hasNextPage ? "Loading..." : "No search left"}
-        </div>
-      </ul>
+  return (
+    <div className="container m-auto p-6 h-screen overflow-hidden">
+      <header className="mb-6 flex justify-between">
+        <h1 className="font-bold">My Blogs</h1>
+        <input
+          type="text"
+          placeholder="Search"
+          className="border-2 rounded w-96 px-4 py-2"
+          onChange={(e) => setFilterBy(e.target.value)}
+          value={filterBy}
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded"
+          onClick={() => {
+            setSort(!sortPosts);
+          }}
+        >
+          Sort
+        </button>
+      </header>
+      <div className="flex h-full pb-4">
+        <ul className="flex-grow overflow-auto">
+          {isSuccess &&
+            data.pages
+              ?.flat()
+              .filter(
+                ({ title, body }) => title.includes(filterBy)
+                // || body.includes(filterBy)
+              )
+              .sort((a, b) => (sortPosts ? a.title.localeCompare(b.title) : 0))
+              .map(({ id, title }) => (
+                <li
+                  key={id}
+                  className="px-4 py-2 my-4 cursor-pointer border-2 rounded capitalize hover:bg-slate-300"
+                  onClick={() => setSelected(id)}
+                >
+                  {title}
+                </li>
+              ))}
+
+          <div className="loader" ref={observerElem}>
+            {isFetchingNextPage && hasNextPage
+              ? "Loading..."
+              : "No search left"}
+          </div>
+        </ul>
+        {selected > -1 &&
+          isSuccess &&
+          data.pages
+            .flat()
+            .filter(({ id }) => id == selected)
+            .map(({ title, body }) => (
+              <div className="p-2 rounded border-2 m-4 w-1/2">
+                <h1 className="font-bold capitalize mb-4">{title}</h1>
+                <p>{body}</p>
+              </div>
+            ))}
+      </div>
     </div>
   );
 }
--- utils/apiUtils.ts
+++ utils/apiUtils.ts
@@ -1,4 +1,4 @@
-const LIMIT = 5;
+const LIMIT = 10;
 export function fetchPosts(page: number) {
   return fetch(
     `https://jsonplaceholder.typicode.com/posts?_start=${
@@ -6,3 +6,10 @@ export function fetchPosts(page: number) {
     }&_limit=${LIMIT}`
   ).then((res) => res.json());
 }
+
+export type postType = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
